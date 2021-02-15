cc280912369dcb0248cd98b7d66504f249a687bf
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-02-15 11:30:26 +0100

    Actualizar Board.java, Reservation.java y Turn.java se mapean las clases que faltaban en las entidades y se comprueba el correcto despliegue de la applicacion

--- src/main/java/com/boot/bookingrestaurantapi/entities/Reservation.java
+++ src/main/java/com/boot/bookingrestaurantapi/entities/Reservation.java
@@ -1,5 +1,47 @@
 package com.boot.bookingrestaurantapi.entities;
 
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("deprecation")
+@Entity
+@Table(name="RESERVATION")
+@Getter
+@Setter
 public class Reservation {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private Long id;
+	
+	@Column(name="LOCATOR")
+	private String locator;
+	
+	@Column(name="PERSON")
+	private Long person;
+	
+	@Column(name="DATE")
+	private Date date;
+	
+	@Column(name="TURN")
+	private String turn;
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name = "RESTAURANT_ID",nullable = false)
+	private Restaurant restaurant;
 }
--- src/main/java/com/boot/bookingrestaurantapi/entities/Turn.java
+++ src/main/java/com/boot/bookingrestaurantapi/entities/Turn.java
@@ -1,5 +1,37 @@
 package com.boot.bookingrestaurantapi.entities;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("deprecation")
+@Entity
+@Table(name = "TURN")
+@Getter
+@Setter
 public class Turn {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID",nullable = false)
+	private Long id;
+	
+	@Column(name = "NAME")
+	private String name;
+	
+	@ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	@JoinColumn(name = "RESTAURANT_ID",nullable = false)
+	private Restaurant restaurant;
 }
+
