03c29dcebe06ee3dd0d17dcb0f269e36ba5c3000
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-02-15 10:27:17 +0100

    Actualizar pom.xml, Board.java y 3 más archivos... Se crean las clases de entidades de nuestra base de datos y se mapea la clase Restaurant

---
+++ src/main/java/com/boot/bookingrestaurantapi/entities/Restaurant.java
@@ -0,0 +1,51 @@
+package com.boot.bookingrestaurantapi.entities;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "RESTAURANT")
+@Getter
+@Setter
+public class Restaurant {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@Column(name = "ID", unique = true, nullable = false)
+	private Long id;
+	
+	@Column(name = "NAME")
+	private String name;
+	
+	@Column(name = "DESCRIPTION")
+	private String description;
+	
+	@Column(name = "LOCATOR")
+	private String locator;
+	
+	@Column(name = "IMAGE")
+	private String image;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "restaurant")
+	private List<Reservation> reservations;
+	
+	@OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "restaurant")
+	private List<Board> boards;
+	
+	@OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "restaurant")
+	private List<Turn> turns;
+	
+}
+
