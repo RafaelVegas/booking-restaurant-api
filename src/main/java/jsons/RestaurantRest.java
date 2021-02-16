bc16ed144fca910e636cd6b184ea67cd54ecbe60
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-02-16 11:58:19 +0100

    Actualizar RestaurantRest.java Se le añaden los Getter y Setter de lombok

--- src/main/java/jsons/RestaurantRest.java
+++ src/main/java/jsons/RestaurantRest.java
@@ -5,6 +5,11 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RestaurantRest {
 
