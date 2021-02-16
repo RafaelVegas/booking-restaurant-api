dfc21066c76111bb07090ddf0e96ff1f75cdc8e0
RafaelVegas <46669090+RafaelVegas@users.noreply.github.com>
2021-02-17 00:02:37 +0100

    Revert "Feature/create exception"

--- src/main/java/exceptions/InternalServerErrorException.java
+++ src/main/java/exceptions/InternalServerErrorException.java
@@ -10,11 +10,11 @@ public class InternalServerErrorException extends BookingException{
 
 	private static final long serialVersionUID = 1L;
 
-	public InternalServerErrorException(String code, List<ErrorDto> errorList, String message) {
+	public InternalServerErrorException(String code, int responseCode, List<ErrorDto> errorList, String message) {
 		super(code, HttpStatus.INTERNAL_SERVER_ERROR.value(), errorList, message);
 	}
 	
-	public InternalServerErrorException(String code,  String message) {
+	public InternalServerErrorException(String code, int responseCode,  String message) {
 		super(code, HttpStatus.INTERNAL_SERVER_ERROR.value(), message);
 	}
 }
