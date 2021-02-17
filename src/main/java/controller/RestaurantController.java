8e4391dbb6d66133cf92c099bd59ff0420d83128
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-02-17 17:46:25 +0100

    Actualizar RestaurantController.java Se crea el primer controlador

--- src/main/java/controller/RestaurantController.java
+++ src/main/java/controller/RestaurantController.java
@@ -1,5 +1,24 @@
 package controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import exceptions.BookingException;
+import service.implement.RestaurantService;
+
+@RestController
+@RequestMapping("/booking-restaurant" + "v1.0")
 public class RestaurantController {
+	
+	@Autowired
+	private RestaurantService restaurantService;
+	
+	private static ModelMapper modelMapper = new ModelMapper();
+	
+	public RestaurantService getRestaurantService(Long id) throws BookingException {
+		return modelMapper.map(restaurantService.getRestaurantById(id), RestaurantService.class);
+	}
 
 }
