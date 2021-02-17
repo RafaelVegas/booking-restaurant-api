f5c1a90010e25779e28096fb9486e58b7f50782a
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-02-17 11:00:37 +0100

    Actualizar CreateReservationRest.java Se añaden sus getter y setter

--- src/main/java/jsons/CreateReservationRest.java
+++ src/main/java/jsons/CreateReservationRest.java
@@ -5,6 +5,11 @@ import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CreateReservationRest {
 
