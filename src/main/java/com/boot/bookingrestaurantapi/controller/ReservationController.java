882c6126aad2527fd0a054ee4a8f8bdce61ebc57
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-02-18 18:37:45 +0100

    Se crea el controlador de crear una reserva y consultar una reserva

--- src/main/java/com/boot/bookingrestaurantapi/controller/ReservationController.java
+++ src/main/java/com/boot/bookingrestaurantapi/controller/ReservationController.java
@@ -1,5 +1,40 @@
 package com.boot.bookingrestaurantapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boot.bookingrestaurantapi.exceptions.BookingException;
+import com.boot.bookingrestaurantapi.jsons.CreateReservationRest;
+import com.boot.bookingrestaurantapi.jsons.ReservationRest;
+import com.boot.bookingrestaurantapi.responses.BookingRestaurantResponse;
+import com.boot.bookingrestaurantapi.service.implement.ReservationService;
+
+@RestController
+@RequestMapping(path = "/reservation-restaurant" + "/v1.0")
 public class ReservationController {
 
+	@Autowired
+	ReservationService reservationService;
+
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(method = RequestMethod.POST, value = "create-reservation", produces = MediaType.APPLICATION_JSON_VALUE)
+	public BookingRestaurantResponse<String> createReservation(
+			@RequestBody CreateReservationRest createReservationRest) throws BookingException {
+		return new BookingRestaurantResponse<String>("SUCESS", String.valueOf(HttpStatus.OK), "Reserva creada",
+				reservationService.createReservation(createReservationRest));
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(method = RequestMethod.GET, value = "consult-reservation", produces = MediaType.APPLICATION_JSON_VALUE)
+	public BookingRestaurantResponse<ReservationRest> findReservation(@PathVariable Long id) throws BookingException {
+		return new BookingRestaurantResponse<>("SUCESS", String.valueOf(HttpStatus.OK), "Consulta realizada",
+				reservationService.getReservation(id));
+	}
 }
