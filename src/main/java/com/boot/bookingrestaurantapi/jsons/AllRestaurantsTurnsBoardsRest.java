9947ef35760b8c35c215791b0eef73e1cba20e99
FaliVegas <rafael.vegas.lanzat@gmail.com>
2021-02-18 11:30:24 +0100

    Actualizar AllRestaurantsTurnsBoardsRest.java Se refactoriza la clase

--- src/main/java/com/boot/bookingrestaurantapi/jsons/AllRestaurantsTurnsBoardsRest.java
+++ src/main/java/com/boot/bookingrestaurantapi/jsons/AllRestaurantsTurnsBoardsRest.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class RestaurantRest {
+public class AllRestaurantsTurnsBoardsRest {
 
 	@JsonProperty("id")
 	private Long id;
